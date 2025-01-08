490f38d14300f4427b1747edae96eeadf0e6317d
gllmsb <val29s03@gmail.com>
2025-01-08 14:34:11 +0100

    footer and npm react icons

--- Bageriet/src/components/Footer/Footer.jsx
+++ Bageriet/src/components/Footer/Footer.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
+import { IoIosArrowUp } from "react-icons/io";
 import styles from './Footer.module.scss';
 
 export const Footer = () => {
+  // Function to scroll to the top of the page
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContent}>
-        <h2 className={styles.footerTitle}>bageriet</h2>
+        <h1 className={styles.footerTitle}>bageriet</h1>
         <p className={styles.footerDescription}>
-          Der er mange tilgængelige udgaver af Lorem Ipsum, men de fleste udgaver har gennemgået
-          forandringer, når nogen har tilføjet humor eller tilfældige ord, som på ingen måde ser ægte ud.
+          Der er mange tilgængelige udgaver af Lorem Ipsum, men de fleste udgaver har
+          gennemgået forandringer, når nogen har tilføjet humor eller tilfældige ord, som
+          på ingen måde ser ægte ud.
         </p>
       </div>
-      <div className={styles.footerCopyright}>
+      <div className={styles.footerBottom}>
         <p>Copyright © 2017 bageriet aps</p>
       </div>
+      <button className={styles.backToTopButton} onClick={scrollToTop}>
+      <IoIosArrowUp />
+      </button>
     </footer>
   );
 };
