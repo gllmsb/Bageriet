996a7ab87c9241aa6bd396984a7bf3d68a85ae31
gllmsb <val29s03@gmail.com>
2025-01-08 14:12:43 +0100

    footer

--- Bageriet/src/components/Footer/Footer.jsx
+++ Bageriet/src/components/Footer/Footer.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from 'react';
+import styles from './Footer.module.scss';
 
 export const Footer = () => {
   return (
-    <div>footer</div>
-  )
-}
+    <footer className={styles.footer}>
+      <div className={styles.footerContent}>
+        <h2 className={styles.footerTitle}>bageriet</h2>
+        <p className={styles.footerDescription}>
+          Der er mange tilgængelige udgaver af Lorem Ipsum, men de fleste udgaver har gennemgået
+          forandringer, når nogen har tilføjet humor eller tilfældige ord, som på ingen måde ser ægte ud.
+        </p>
+      </div>
+      <div className={styles.footerCopyright}>
+        <p>Copyright © 2017 bageriet aps</p>
+      </div>
+    </footer>
+  );
+};
